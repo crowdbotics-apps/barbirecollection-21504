9aa67d8fcef8cdc019ef8d9c661631f5ef60f2fd
Crowdbotics <team@crowdbotics.com>
2020-10-14 20:07:00 +0000

    add package blueprint-react-native-articles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'ArticleList142853', human_name: 'Article List', access_route: 'ArticleList142853'},
 { name: 'Maps142840', human_name: 'Maps', access_route: 'Maps142840', icon: 'map'},
 
   // you can add more installed blueprints here
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import ArticleList142853Navigator from '../features/ArticleList142853/navigator';
 import Maps142840Navigator from '../features/Maps142840/navigator';
 
 /**
@@ -14,6 +15,7 @@ import Maps142840Navigator from '../features/Maps142840/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+ArticleList142853: { screen: ArticleList142853Navigator },
 Maps142840: { screen: Maps142840Navigator },
 
     /** new navigators can be added here */
