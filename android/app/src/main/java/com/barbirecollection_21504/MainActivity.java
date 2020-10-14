51189ab534f1011ae12d9af945efb79c497a008d
Crowdbotics <team@crowdbotics.com>
2020-10-14 20:04:43 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/barbirecollection_21504/MainActivity.java
@@ -0,0 +1,15 @@
+package com.barbirecollection_21504;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "barbirecollection_21504";
+    }
+}
